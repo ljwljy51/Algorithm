45dbedccb69bdaea54be79a292b35179d756b26f
ljwljy51 <84127377+ljwljy51@users.noreply.github.com>
2023-02-14 11:13:40 +0900

    Add files via upload

---
+++ boj/G4_1339.py
@@ -0,0 +1,30 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+word_lst=[]
+
+for i in range(n):
+    word_lst.append(input().strip())
+
+dict={}
+for i in range(n): #모든 단어에 대해
+    for j in range(len(word_lst[i])): #단어 길이만큼
+        if word_lst[i][j] in dict: #해당 알파벳이 딕셔너리에 있으면
+            dict[word_lst[i][j]]+=10**(len(word_lst[i])-j-1) #가중치 더해줌
+        else: #딕셔너리에 없는 경우
+            dict[word_lst[i][j]]=10**(len(word_lst[i])-j-1)
+        
+weight_lst=[]
+for n in dict.values():
+    weight_lst.append(n)
+
+weight_lst.sort(reverse=True)
+
+num=9 #9~0 . 승수
+answer=0
+for weight in weight_lst:
+    answer+=(num*weight) #수 계산
+    num-=1
+
+print(answer)
---
+++ boj/G5_11000.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+input=sys.stdin.readline
+
+n=int(input())
+rooms=[]
+
+for i in range(n):
+    start, end=map(int, input().split())
+    rooms.append((start, end))
+    
+rooms.sort() #시작 시간 기준으로 정렬
+
+end_time=[]
+heapq.heappush(end_time, rooms[0][1]) #첫 번쨰 강의의 끝나는 시간 넣어둠
+
+for i in range(1,n):
+    if rooms[i][0]<end_time[0]: #현재 강의 끝나는 시간보다 다음 강의 시작 시간이 빠르면
+        heapq.heappush(end_time, rooms[i][1]) #새로운 강의실 필요
+    else: #현재 강의에 이어서 할 수 있으면
+        heapq.heappop(end_time) #기준 다시 잡기 위함
+        heapq.heappush(end_time, rooms[i][1]) #강의 끝나는 시간 넣어줌
+        
+        
+print(len(end_time))
