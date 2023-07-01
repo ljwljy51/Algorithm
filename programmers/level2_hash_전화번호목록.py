bf375781ccb138a5e70473af9335adb0d13c5458
ljwljy51 <84127377+ljwljy51@users.noreply.github.com>
2023-07-01 17:59:35 +0900

    Create level2_hash_전화번호목록.py

---
+++ programmers/level2_hash_전화번호목록.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort()  # 정렬
+    current_num = phone_book[0]  # 비교 기준
+    for i in range(1, len(phone_book)):
+        print(current_num, phone_book[i])
+        if current_num == phone_book[i][: len(current_num)]:  # 만약 접두사에 해당할 경우
+            return False
+        else:  # 접두사 아닐 경우
+            current_num = phone_book[i]
+
+    return answer
