aec877f23dca2e772ad0bad16ad6ddb595eb57b3
ljwljy51 <84127377+ljwljy51@users.noreply.github.com>
2023-07-01 17:23:52 +0900

    Create level2_stackQ_올바른괄호.py

---
+++ programmers/level2_stackQ_올바른괄호.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+
+def solution(s):
+    dq = deque()  # 스택 선언
+    for par in s:
+        try:
+            dq.append(par) if par == "(" else dq.pop()
+        except:  # 빈 스택에서 꺼내려고 할 경우
+            return False
+
+    return True if len(dq) == 0 else False  # 남아있는 요소가 있으면 false
