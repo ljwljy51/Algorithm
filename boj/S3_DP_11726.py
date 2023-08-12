e1456257118b331b8e4e1330f69bfd1f6b4d8009
ljwljy51 <84127377+ljwljy51@users.noreply.github.com>
2023-08-12 14:22:50 +0900

    Create S3_DP_11726.py

---
+++ boj/S3_DP_11726.py
@@ -0,0 +1,10 @@
+#https://www.acmicpc.net/problem/11726
+#DP
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [1, 2]  # n=1, n=2에 대한 값 초기화
+for i in range(2, n):
+    dp.append(dp[i-2] + dp[i-1])
+
+print(dp[n-1] % 10007)
