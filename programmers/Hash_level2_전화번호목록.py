d24a6e82c93e8122e7699fb4cff71f67bb7c8a03
ljwljy51 <84127377+ljwljy51@users.noreply.github.com>
2023-09-14 17:04:34 +0900

    Update and rename level2_hash_전화번호목록.py to Hash_level2_전화번호목록.py

--- programmers/Hash_level2_전화번호목록.py
+++ programmers/Hash_level2_전화번호목록.py
@@ -1,9 +1,9 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42577
 def solution(phone_book):
     answer = True
     phone_book.sort()  # 정렬
     current_num = phone_book[0]  # 비교 기준
     for i in range(1, len(phone_book)):
-        print(current_num, phone_book[i])
         if current_num == phone_book[i][: len(current_num)]:  # 만약 접두사에 해당할 경우
             return False
         else:  # 접두사 아닐 경우
