b4101df2bd010bb513b14aa704f6cfada39e4387
ljwljy51 <ljwljy51@naver.com>
2023-12-14 22:17:02 +0900

    upload files

---
+++ Programmers/SQL/StringDate/Lv3_대여기록이존재하는자동차리스트구하기.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157341
+SELECT
+    A.CAR_ID
+FROM
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY AS A
+    LEFT JOIN CAR_RENTAL_COMPANY_CAR AS B ON A.CAR_ID = B.CAR_ID
+WHERE
+    B.CAR_TYPE = '세단'
+    AND A.START_DATE LIKE '%-10-%'
+GROUP BY
+    CAR_ID
+ORDER BY
+    CAR_ID DESC;
